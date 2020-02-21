921c989961355cc1de56de4bf0b55f505071f0b1
Amster <amstergo46@gmail.com>
2020-02-21 10:46:44 +0300

    small fix

--- src/components/ActiveQuiz/index.js
+++ src/components/ActiveQuiz/index.js
@@ -2,25 +2,32 @@ import React from "react";
 import styles from "./index.module.css";
 import AnswersList from "./AnswersList";
 
-const ActiveQuiz = props => (
-  <div className={styles.ActiveQuiz}>
-    <p className={styles.Question}>
-      <span>
-        <strong>{props.answerNumber}&nbsp;</strong>
-        {props.question}
-      </span>
-      <small>
-        {props.answerNumber} из {props.quizLength}
-      </small>
-    </p>
+const ActiveQuiz = props => {
+  return (
+    <div className={styles.ActiveQuiz}>
+      <p className={styles.Question}>
+        <span>
+          <strong>{props.answerNumber}&nbsp;</strong>
+          {props.question}
+        </span>
+        <small>
+          {props.answerNumber} из {props.quizLength}
+        </small>
+      </p>
 
-    <div className={styles.Body}>{props.bodyQuestion}</div>
-    <AnswersList
-      state={props.state}
-      answers={props.answers}
-      onAnswerClick={props.onAnswerClick}
-    />
-  </div>
-);
+      {props.bodyQuestion !== undefined ? (
+        <>
+          <hr style={{ borderColor: "#ffffff21" }} />
+          <div className={styles.Body}>{props.bodyQuestion}</div>
+        </>
+      ) : null}
 
+      <AnswersList
+        state={props.state}
+        answers={props.answers}
+        onAnswerClick={props.onAnswerClick}
+      />
+    </div>
+  );
+};
 export default ActiveQuiz;
